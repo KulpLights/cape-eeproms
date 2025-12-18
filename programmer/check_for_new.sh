a8d6524e1a6c3f7cf534c1aef4d52a4b5d507739
Daniel Kulp <dan@kulp.com>
2025-12-18 15:59:47 -0500

    Initial commit

---
+++ programmer/check_for_new.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd /home/fpp/media/cape-eeproms
+git pull -f
+
+if [ -f /dev/mmcblk0p1 ]; then
+    mount -t auto /dev/mmcblk0p1 /mnt
+    cp -f /mnt/k* /home/fpp/media/cape-eeproms/
+    if [ -f /home/fpp/media/cape-eeproms/kulp-programmer.tgz ]; then
+        cd /home/fpp/media/cape-eeproms
+        tar -xzf /home/fpp/media/cape-eeproms/kulp-programmer.tgz
+        rm -f /home/fpp/media/cape-eeproms/kulp-programmer.tgz
+    fi
+    chown -R fpp:fpp /home/fpp/media/cape-eeproms/*
+    chmod +x /home/fpp/media/cape-eeproms/programmer/programmer
+    chmod +x /home/fpp/media/cape-eeproms/programmer/write_cape_eeprom
+    chmod +x /home/fpp/media/cape-eeproms/programmer/*.sh
+    umount /mnt
+fi 
